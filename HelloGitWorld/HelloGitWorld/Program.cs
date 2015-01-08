8825b559a2773fd43fbc41cfa6316c9865ce9627
stevemurch <ceo@bigoven.com>
2015-01-08 11:40:18 -0800

    Adding a new class, and trying to ignore files

--- HelloGitWorld/HelloGitWorld/Program.cs
+++ HelloGitWorld/HelloGitWorld/Program.cs
@@ -11,8 +11,9 @@ namespace HelloGitWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Git World. Press a key to continue...");
-            Console.ReadKey();
             Console.WriteLine("Wow, did this work?");
+            Console.ReadKey();
         }
+
     }
 }
